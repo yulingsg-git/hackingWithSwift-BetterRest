05cda75cc933b50b2f4178dd953cd1396ee1d80d
Yuling <yuling.sg@gmail.com>
2026-05-22 14:57:35 +0900

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Chen Yuling on 2026/05/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
